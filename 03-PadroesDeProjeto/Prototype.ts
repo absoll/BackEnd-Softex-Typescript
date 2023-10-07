f3d915fcbaba90dd33ee5b943034207afe91a86c
absoll <uzumakijunior@gmail.com>
2023-10-07 05:49:10 +0000

    Padrao Prototype Finalziado

--- 03-PadroesDeProjeto/Prototype.ts
+++ 03-PadroesDeProjeto/Prototype.ts
@@ -20,80 +20,78 @@ https://stackoverflow.com/questions/28150967/typescript-cloning-object/28152032#
 
 */
 
-abstract class Veiculo{
-    modelo:string
-    marca:string
-    cor:string
+abstract class Veiculo {
+    modelo: string
+    marca: string
+    cor: string
     numeroRodas: number
 
-    constructor(modelo:string, marca:string, cor:string, numeroRodas: number){
+    constructor(modelo: string, marca: string, cor: string, numeroRodas: number) {
         this.modelo = modelo
         this.marca = marca
         this.cor = cor
         this.numeroRodas = numeroRodas
     }
 
-    represent(){
+    represent() {
         console.log(`Modelo: ${this.modelo}`)
         console.log(`Marca: ${this.marca}`)
         console.log(`Cor: ${this.cor}`)
         console.log(`Numero de Rodas: ${this.numeroRodas}`)
     }
 
-    clone() {
+    clone(): any {
         //é como se fosse "Object.assign(new MOTO(), this)"
         return Object.assign(eval(`new ${(this as any).constructor.name}()`), this) // -> MELHOR MODO. Cria objetos independentes e ESPECIFICA o tipo de cada um, assim pode chamar seus metodos
-      // return Object.assign({}, this) // -> Cria objetos GENERICOS INDEPENDENTES. Alterar a informação de um não modifica o do outro, porém esses objetos nao tem metodos
-       // return Object.assign(this) // shallow copy -> Cria objetos "interligados". Ao mudar o valor de um deles, muda o dos outros. Não possui os metodos da classe ('represent' nao funciona)
-       //  return JSON.stringify(this); //deep copy -> Retorna um JSON com os dados do objeto. Util se quiser criar um novo objeto com esses dados
+        // return Object.assign({}, this) // -> Cria objetos GENERICOS INDEPENDENTES. Alterar a informação de um não modifica o do outro, porém esses objetos nao tem metodos
+        // return Object.assign(this) // shallow copy -> Cria objetos "interligados". Ao mudar o valor de um deles, muda o dos outros. Não possui os metodos da classe ('represent' nao funciona)
+        //  return JSON.stringify(this); //deep copy -> Retorna um JSON com os dados do objeto. Util se quiser criar um novo objeto com esses dados
         //return Object.create(this) //-> parecido com o assign. Cria de fato um novo objeto, mas esse novo objeto tem os atributos do "pai". Se mudar os dados do atributo Pai, os clones deles sao mudados tambem
-       
+
         //OBS:
         // (this as any).constructor.name} -> Pega o NOME DA CLASSE do objeto, assim usando EVAL pode criar uma nova classe usando esses nome obtido. o THIS envia os dados para o construtor. Ele cria um OBJETO NOVO com os dados do THIS
     }
-
-
 }
 
-class Carro extends Veiculo{
+class Carro extends Veiculo {
     numeroPortas: number
 
-    constructor(modelo:string, marca:string, cor:string, numeroRodas: number, numeroPortas: number){
+    constructor(modelo: string, marca: string, cor: string, numeroRodas: number, numeroPortas: number) {
         super(modelo, marca, cor, numeroRodas)
         this.numeroPortas = numeroPortas
     }
 
-    represent(){
-        console.log("TIpo Veiculo: Carro")
+    represent() {
+        console.log("Tipo Veiculo: Carro")
         super.represent()
         console.log(`Numero de Portas: ${this.numeroPortas}`)
     }
 
 }
 
-class Moto extends Veiculo{
+class Moto extends Veiculo {
     cilindradas: number
 
-    constructor(modelo:string, marca:string, cor:string, numeroRodas: number, cilindradas: number){
+    constructor(modelo: string, marca: string, cor: string, numeroRodas: number, cilindradas: number) {
         super(modelo, marca, cor, numeroRodas)
         this.cilindradas = cilindradas
     }
 
-    represent(){
-        console.log("TIpo Veiculo: Moto")
+    represent() {
+        console.log("Tipo Veiculo: Moto")
         super.represent()
         console.log(`Cilindradas: ${this.cilindradas}`)
     }
 }
 
 
-class Aplicacao{
+class Aplicacao {
 
-   // constructor(){
-   //     this.lista()
-   // }
+    // constructor(){
+    //     this.lista()
+    // }
 
-    static lista(): Array<Veiculo>{
+    static lista(): Array<Veiculo> {
         let lista: Array<Veiculo> = []
 
         let carro: Carro = new Carro('ModeloCarro1', "MarcaCarro1", "CorCarro1", 4, 2)
@@ -112,7 +110,7 @@ class Aplicacao{
 
         moto2.cor = "nenhuma"               //mudando alguns dados
         moto.modelo = "mode"                //mudando alguns dados
-        
+
         lista.push(carro, carro2, carro3, moto, moto2, moto3)
 
         return lista
@@ -122,9 +120,9 @@ class Aplicacao{
 
 let listaVeiculos = Aplicacao.lista()
 
-console.log(listaVeiculos)
+//console.log(listaVeiculos)
 
-for(let i = 0; i < listaVeiculos.length; i++){
+for (let i = 0; i < listaVeiculos.length; i++) {
     //console.log(listaVeiculos[i])
     //console.log(listaVeiculos[i].constructor.name)
     listaVeiculos[i].represent()
